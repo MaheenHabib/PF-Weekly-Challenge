cc32822ae12a06e9de7b0271b70123d236c2e7a2
MaheenHabib <maheenhabib05@gmail.com>
2024-12-05 00:20:16 +0500

    DONE

---
+++ Calculator 2.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <cmath>   // For sq root and power ftns
+using namespace std;
+
+int main() {
+    double num1, num2, result;
+    char op, repeat = 'y';
+
+    while (repeat == 'y' || repeat == 'Y') 
+    {
+        cout << "Enter first number:"<<endl;
+        cin >> num1;
+        cout << "Enter an operator (+, -, *, /, ^ for power, r for square root, % for percentage):"<<endl;
+        cin >> op;
+
+        if (op == 'r') 
+        { // Square root
+            cout << "Square root of " << num1 << " is: " << sqrt(num1) << endl;
+        }
+         
+        else if (op == '%') 
+        { // Percentage calculation
+            cout << "Enter percentage value:"<<endl;
+            cin >> num2;
+            result = (num1 * num2) / 100;
+            cout << num2 << "% of " << num1 << " is: " << result << endl;
+        }
+          
+         else if (op == '+')
+         {
+            cout << "Enter second number:"<<endl;
+            cin >> num2;
+            result = num1 + num2;
+            cout<<"Result:"<<result<<endl;
+         }     
+         else if (op == '-') 
+         {
+            cout << "Enter second number:"<<endl;
+            cin >> num2;
+            result = num1 - num2;
+            cout<<"Result:"<<result<<endl;
+         }
+            
+         else if (op == '*') 
+         {  
+            cout << "Enter second number:"<<endl;
+            cin >> num2;
+            result = num1 * num2;
+            cout<<"Result:"<<result<<endl;
+         }
+             
+         else if (op == '/') 
+         {  
+            cout << "Enter second number:"<<endl;
+            cin >> num2;
+             if (num2 != 0) 
+               {
+                  result = num1 / num2;
+                  cout <<"Result:"<< result << endl;
+               }
+             else
+               {
+                  cout << "Error! Division by zero."<<endl;
+                  continue;
+               }
+         }
+            
+         else if (op == '^')
+         { // Exponentiation (Power)
+            cout << "Enter second number:"<<endl;
+            cin >> num2;
+            result = pow(num1, num2);
+            cout << "Result:" << result << endl;
+         } 
+          
+         else 
+         {
+            cout << "Invalid operator! Try again."<<endl;
+            continue;
+         }
+
+        cout << "Do you want to perform another calculation? (y/n): ";
+        cin >> repeat;
+    }
+
+    cout << "Goodbye!" << endl;
+
+    return 0;
+}
